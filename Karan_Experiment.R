034d57713d5c32f766ae6247e8383ffd9df196fa
Karan Mehta <karan.mehta@student.uts.edu.au>
2019-09-08 14:30:21 +1000

    updates

--- Karan_Experiment.R
+++ Karan_Experiment.R
@@ -17,7 +17,7 @@ library(stringdist)
 library(stringr)
 library(edgarWebR)
 library(spacyr)
-#spacy_install()
+#spacy_install()  ### Install if you have not previously Installed this package
 
 registerDoParallel(detectCores())
 
@@ -90,62 +90,81 @@ combinedListing <- distinct(combinedListing)
 combinedListing <- combinedListing %>%
   filter(combinedListing$MarketCap != "n/a")
 
-##############################
+#############################################
 
 
-############## Entity Extraction for Databreach dataset ################
+############## ORG Matching ################
 
 spacy_initialize()
 dataTemp <- data
-compData <- data.frame()
+
+# Create a new column in combinedListing with the company name cleaned up
+combinedListing <- cbind(combinedListing, clean = gsub(' Incorporated| Corporated| Corporation', '', combinedListing$Name))
+combinedListing$clean <- gsub('the', '', combinedListing$clean, ignore.case = TRUE)
+combinedListing$clean <- gsub(', Inc|, Inc.| Inc| Inc.| Corp|, Corp| Corp.|, Corp.| Ltd.| Ltd', '', combinedListing$clean)
+combinedListing$clean <- gsub('\\(The\\)|[.]|\'|,', '', combinedListing$clean)
+combinedListing$clean <- str_replace_all(combinedListing$clean,"[^a-zA-Z\\s]", " ")
+
+# copy data over from dataTemp - only to use while de-bugging. Can be deleted in final version
 data <- dataTemp
+
+# Create a new column in data with the Compnay name cleaned up
+
 data <- cbind(data, clean = gsub(' Incorporated| Corporated| Corporation', '', data$Company))
 data$clean <- gsub('the', '', data$clean, ignore.case = TRUE)
 data$clean <- gsub(', Inc|, Inc.| Inc| Inc.| Corp|, Corp| Corp.|, Corp.| Ltd.| Ltd', '', data$clean)
 data$clean <- gsub('\\(The\\)|[.]|\'|,', '', data$clean)
+data$clean <- str_replace_all(data$clean,"[^a-zA-Z\\s]", " ")
 
+
+# proc time to measure how long the loop runs for.
 ptm <- proc.time()
-for(rows in 1:nrow(data)){
-  rowComp <- data$clean[rows]
-  kw <- spacy_extract_entity(rowComp, output = "list", type = "named", multithread = TRUE)
-  if(length(kw)==1){
+
+for(rows in 1:nrow(data)){ #for all rows in the databreach set.
+  rowComp <- data$clean[rows] # searh on the cleaned company name
+  kw <- spacy_extract_entity(rowComp, output = "list", type = "named", multithread = TRUE) # use spacy to extract names entities as a list
+  if(length(kw)==1){ # if list is of length 1, then use the comany name as keyword for lookup
     kw <- rowComp
   }
-  data$KW[rows] <- kw
+  data$KW[rows] <- kw # add column for kw used in searching
 
   # dl and 3.5 = 218 hits
   # lcs and 2.5 = 115 hits*
   
-  # Match on compnay name in combined listings
-   myMatch <- amatch(kw, combinedListing$Name, nomatch = 0, maxDist = 2.5, nthread = 7, method = "lcs")
-  # 
-  # # Match on first word in the listings name against companmy name where company name is less than = 2 words
-  # 
-   myMatch2 <- amatch(kw, word(combinedListing$Name,1,2), nomatch = 0, maxDist = 1, nthread = 7, method = "lcs")
-  # 
-  # # match on stock symbols
-   myMatch3 <- amatch(kw, combinedListing$Symbol, nomatch = 0,  maxDist = .1, nthread = 7, method = "lcs")
+  # Match on first word in the listings name against companmy name where company name is less than = 2 words
+   myMatch <- amatch(kw, word(combinedListing$clean,1,1), nomatch = 0, maxDist = 1, nthread = 7, method = "lcs")
+   
+  # match on stock symbols
+   myMatch2 <- amatch(kw, combinedListing$Symbol, nomatch = 0,  maxDist = 1, nthread = 7, method = "dl")
+   
+   # Match on compnay name in combined listings
+   myMatch3 <- amatch(kw, combinedListing$clean, nomatch = 0, maxDist = 3.5, nthread = 7, method = "dl")
   
   if(myMatch != 0){
     data$CompanyName[rows] <- combinedListing$Name[myMatch]
     data$stockTicker[rows] <- combinedListing$Symbol[myMatch]
+    data$match[rows] <- c("Match 1")
   }
   else if(myMatch == 0 && myMatch2 != 0){
       data$CompanyName[rows] <- combinedListing$Name[myMatch2]
       data$stockTicker[rows] <- combinedListing$Symbol[myMatch2]
+      data$match[rows] <- c("Match 2")
   }
   else if(myMatch == 0 && myMatch2 == 0 && myMatch3 != 0){
       data$CompanyName[rows] <- combinedListing$Name[myMatch3]
       data$stockTicker[rows] <- combinedListing$Symbol[myMatch3]
+      data$match[rows] <- c("Match 3")
   }
   else{
       data$CompanyName[rows] <- c("Not Listed")
-      data$stockTicker[rows] <- c("NA")
+      data$stockTicker[rows] <- NA
+      data$match[rows] <- NA
   }
 
 }
 proc.time()-ptm
 
+# Create dataframe 'examine' to check the results of the matching excercise.
 examine <- data %>%
   filter(stockTicker != "NA") %>%
-  select(Company, BreachType, CompanyName, stockTicker)
+  select(Company, BreachType, clean, CompanyName, stockTicker, match)
