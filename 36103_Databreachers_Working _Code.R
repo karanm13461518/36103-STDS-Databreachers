3b2831d605b70a9999615008501517a116bc1350
Karan Mehta <karan.mehta@student.uts.edu.au>
2019-09-10 19:17:15 +1000

    Adding new Script for group contribution

---
+++ 36103_Databreachers_Working _Code.R
@@ -0,0 +1,92 @@
+#### Load the libraries
+library(tidyr)
+library(tidyverse)
+library(forcats)
+library(dplyr)
+library(caret)
+library(jsonlite)
+library(gtrendsR)
+library(lubridate)
+library(tabulizer)
+library(countrycode)
+library(svMisc)
+library(foreach)
+library(doParallel)
+library(tidystringdist)
+library(stringdist)
+library(stringr)
+library(edgarWebR)
+library(spacyr)
+#spacy_install()  ### Install if you have not previously Installed this package
+
+registerDoParallel(detectCores())
+
+#### Breaches CSV Files
+## Read in the CSV file. Some warning which appear can be ignored.
+dataPart1 <- read_csv("data/Privacy_Rights_Clearinghouse-Data-Breaches-Export_2005-2012.csv")
+dataPart2 <- read_csv("data/Privacy_Rights_Clearinghouse-Data-Breaches-Export_2013-2019csv.csv")
+
+## Combine the two CSVs and convert to a data frame
+data <- rbind(dataPart1,dataPart2)
+data <- as.data.frame(data)
+
+## Renmae the column headings for our data breaches dataset
+names(data)
+names(data) <- c("DateMadePublic", "Company", "City", "State", "BreachType", "OrgType", "TotalRecords", "Description", "InfoSource","SourceURL", "BreachYear", "Latitude", "Longitude")
+
+## Convert OrgType, Breach Yr, Breack Type and Company name to factor
+data[,c("OrgType", "BreachYear", "BreachType")]<-lapply(data[,c("OrgType", "BreachYear", "BreachType")], as.factor)
+sapply(data, class)
+
+#### Initial High level EDA
+
+## Rename factor level in Breach Type
+
+data <- data %>%
+  mutate(BreachType = fct_recode(BreachType,
+                                 "Hacking" = "HACK",
+                                 "Insider" = "INSD",
+                                 "Card Fraud" = "CARD",
+                                 "Physical Loss" = "PHYS",
+                                 "Portable Device" = "PORT",
+                                 "Stationary Device" = "STAT",
+                                 "Unintended Disclosure" = "DISC",
+                                 "Unknown" = "UNKN"
+  ))
+
+
+## Rename factor level in Org Type
+data <- data %>%
+  mutate(OrgType = fct_recode(OrgType,
+                              "Financial" = "BSF",
+                              "Business Other" = "BSO",
+                              "Online Retailer" = "BSR",
+                              "Educational" = "EDU",
+                              "Government" = "GOV",
+                              "Healthcare" = "MED",
+                              "NGOs" = "NGO",
+                              "Unknown" = "UNKN"))
+
+
+
+######### Listings ###########
+
+listingNASDAQ <- read.csv("data/NASDAQcompanylist.csv")
+listingNYSE <- read.csv("data/NYSEcompanylist.csv")
+
+combinedListing <- listingNASDAQ[,-c(8,9)]
+combinedListing$Ex <- c("NASDAQ")
+listingNYSE$Ex <- c("NYSE")
+combinedListing <- rbind(combinedListing, listingNYSE[,-c(8,9)])
+
+combinedListing$Symbol <- as.character(combinedListing$Symbol)
+combinedListing$Name <- as.character(combinedListing$Name)
+combinedListing$LastSale <- as.numeric(combinedListing$LastSale)
+combinedListing$MarketCap <- as.character(combinedListing$MarketCap)
+combinedListing$IPOyear <- as.character(combinedListing$IPOyear)
+combinedListing$Ex <- as.factor(combinedListing$Ex)
+
+combinedListing <- distinct(combinedListing)
+
+combinedListing <- combinedListing %>%
+  filter(combinedListing$MarketCap != "n/a")
